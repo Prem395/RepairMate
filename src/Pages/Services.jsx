b7ec5fd5de106ffdaae1634154604e1c54376eb4
Prem Singh <premrathod0289@gmail.com>
2025-08-01 12:29:05 +0530

    fix: correct ServicesData import path

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import Navbar from "../Components/Navbar";
-import { SERVICES } from "../data/ServicesData";
+import { SERVICES } from "../data/servicesData";
 
 const Services = () => {
   return (
